872abe4eac2a82729fbc8f0ab205903555c85949
II <no_soap_0@mail.ru>
2024-07-18 17:42:58 +0300

    signature

--- src/Signature.php
+++ src/Signature.php
@@ -41,12 +41,7 @@ abstract class Signature extends ApertureConfig
         } catch (\Throwable $th) {
             http_response_code(404);
 
-            $code = $th->getCode();
-            if ($code==0) {
-                return ['error' => new Error("{$this->request->task} not found", 404)];
-            }
-
-            return ['error' => new Error($th->getMessage(), $code)];
+            return ['error' => new Error("{$this->request->task} not found", 404)];
         }
 
         try {
